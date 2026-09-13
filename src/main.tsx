a4486c550c44bf125f4507079d48d4908fee86ab
vvcnyy <yangwoochan68@gmail.com>
2026-09-13 21:05:44 +0900

    feat: pwa support

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,14 @@ import "./styles.css";
 
 initializeGoogleAnalytics();
 
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js", { updateViaCache: "none" })
+      .then((registration) => registration.update())
+      .catch(() => undefined);
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
